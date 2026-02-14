389055e0ce06733766d6c4f2c8994d71f67c56fa
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-02-14 14:31:49 +0100

    chore: tests and actions

--- src/load.rs
+++ src/load.rs
@@ -1,12 +1,20 @@
 use std::path::Path;
 
+use crate::models::Executor;
 use anyhow::{Result, anyhow};
 use toml::Table;
 
 const JAKEFILE: &str = "jakefile.toml";
 
-pub fn parse_jakefile() -> Result<Table> {
-    let path = Path::new(JAKEFILE);
+pub fn parse_jakefile(file_path: Option<&str>) -> Result<Table> {
+    let owned_path;
+    let path = match file_path {
+        None => Path::new(JAKEFILE),
+        Some(p) => {
+            owned_path = p;
+            Path::new(&owned_path)
+        }
+    };
     if path.exists() {
         let content = std::fs::read_to_string(path)?;
         let table = content.parse::<Table>()?;
@@ -16,7 +24,12 @@ pub fn parse_jakefile() -> Result<Table> {
     }
 }
 
-pub fn execute_command(task: &str, flags: &str) -> Result<()> {
+pub fn execute_command(
+    jakefile_path: Option<&str>,
+    task: &str,
+    flags: &str,
+    executor: &dyn Executor,
+) -> Result<()> {
     if task.is_empty() {
         return Ok(());
     }
@@ -25,7 +38,7 @@ pub fn execute_command(task: &str, flags: &str) -> Result<()> {
     } else {
         flags.split_whitespace().collect()
     };
-    let available_tasks = parse_jakefile()?;
+    let available_tasks = parse_jakefile(jakefile_path)?;
     if !available_tasks.contains_key(task) {
         return Err(anyhow!(
             "Task not available. Please define it within jakefile.toml"
@@ -47,7 +60,7 @@ pub fn execute_command(task: &str, flags: &str) -> Result<()> {
             Some(depends) => {
                 for value in depends {
                     match value.as_str() {
-                        Some(c) => execute_command(c, "")?,
+                        Some(c) => execute_command(jakefile_path, c, "", executor)?,
                         None => continue,
                     }
                 }
@@ -61,36 +74,110 @@ pub fn execute_command(task: &str, flags: &str) -> Result<()> {
     };
     let cmd_parts: Vec<&str> = cmd.split_whitespace().collect();
     let main_command = cmd_parts[0];
-    let mut cmd = if cmd_parts.len() == 1 && cmd_options.is_empty() {
-        std::process::Command::new(main_command)
-            .stdin(std::process::Stdio::inherit())
-            .stdout(std::process::Stdio::inherit())
-            .stderr(std::process::Stdio::inherit())
-            .spawn()?
+    if cmd_parts.len() == 1 && cmd_options.is_empty() {
+        executor.execute(main_command, vec![])?;
     } else if cmd_parts.len() == 1 && !cmd_options.is_empty() {
-        std::process::Command::new(main_command)
-            .args(cmd_options)
-            .stdin(std::process::Stdio::inherit())
-            .stdout(std::process::Stdio::inherit())
-            .stderr(std::process::Stdio::inherit())
-            .spawn()?
+        executor.execute(main_command, cmd_options)?;
     } else if cmd_parts.len() > 1 && cmd_options.is_empty() {
         let cmd_slice = &cmd_parts[1..];
-        std::process::Command::new(main_command)
-            .args(cmd_slice)
-            .stdin(std::process::Stdio::inherit())
-            .stdout(std::process::Stdio::inherit())
-            .stderr(std::process::Stdio::inherit())
-            .spawn()?
+        executor.execute(main_command, cmd_slice.to_vec())?;
     } else {
         let cmd_slice = [&cmd_parts[1..], &cmd_options[..]].concat();
-        std::process::Command::new(main_command)
-            .args(cmd_slice)
-            .stdin(std::process::Stdio::inherit())
-            .stdout(std::process::Stdio::inherit())
-            .stderr(std::process::Stdio::inherit())
-            .spawn()?
-    };
-    cmd.wait()?;
+        executor.execute(main_command, cmd_slice)?;
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::models::CommandExecutor;
+
+    use super::*;
+
+    struct MockCommandExecutor;
+
+    impl MockCommandExecutor {
+        pub fn new() -> Self {
+            Self {}
+        }
+    }
+
+    impl Executor for MockCommandExecutor {
+        fn execute(&self, main_command: &str, args: Vec<&str>) -> anyhow::Result<()> {
+            let full_command = main_command.to_owned() + " " + &args.join(" ");
+            std::fs::write("test.mock", full_command)?;
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_parse_jakefile() {
+        let result = parse_jakefile(Some("testfiles/jakefile.toml"));
+        match result {
+            Err(e) => {
+                println!("An error occurred: {}", e.to_string());
+                assert!(false); // fail here
+            }
+            Ok(t) => {
+                assert!(t.contains_key("say-hello"));
+                assert!(t.contains_key("say-hello-back"));
+                assert!(t.contains_key("say-bye"));
+                assert!(t.contains_key("list"));
+                match t["say-hello"].as_table() {
+                    None => {
+                        println!("say-hello is not a table");
+                        assert!(false); // fail here
+                    }
+                    Some(d) => {
+                        assert!(d.contains_key("command"));
+                        assert!(!d.contains_key("depends_on"));
+                    }
+                }
+                match t["say-bye"].as_table() {
+                    None => {
+                        println!("say-bye is not a table");
+                        assert!(false); // fail here
+                    }
+                    Some(d) => {
+                        assert!(d.contains_key("command"));
+                        assert!(d.contains_key("depends_on"));
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_mock_command_execution() {
+        let mock_executor = MockCommandExecutor::new();
+        let result = execute_command(
+            Some("testfiles/jakefile.toml"),
+            "list",
+            "-la /hello/something",
+            &mock_executor,
+        );
+        assert!(result.is_ok());
+        let mock_content =
+            std::fs::read_to_string("test.mock").expect("Should be able to read test.mock");
+        assert_eq!(mock_content.trim(), "ls -la /hello/something");
+        let result_1 = execute_command(Some("testfiles/jakefile.toml"), "list", "", &mock_executor);
+        assert!(result_1.is_ok());
+        let mock_content_1 =
+            std::fs::read_to_string("test.mock").expect("Should be able to read test.mock");
+        assert_eq!(mock_content_1.trim(), "ls");
+    }
+
+    #[test]
+    fn test_command_execution() {
+        let executor = CommandExecutor::new();
+        let result = execute_command(Some("testfiles/jakefile.toml"), "say-hello", "", &executor);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_command_execution_with_deps() {
+        let executor = CommandExecutor::new();
+        let result = execute_command(Some("testfiles/jakefile.toml"), "say-bye", "", &executor);
+        assert!(result.is_ok());
+    }
 }
